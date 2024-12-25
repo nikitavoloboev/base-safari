712ffa7c3b19a5f5f9c54fd0578475768ff3550f
Nikita <github@nikiv.dev>
2024-12-25 14:47:08 +0100

    .

---
+++ base-safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  base-safari
+//
+//  Created by Nikita on 25/12/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
